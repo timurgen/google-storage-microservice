bc422254872e63136068b6dec761296dfc2e9634
Timur Samkharadze <timur.samkharadze@sysco.no>
2018-11-16 13:13:25 +0100

    more generic file listing, binary file download and upload

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Environment variables
 
-`GOOGLE_APPLICATION_CREDENTIALS` - Path to Google Storage credential file. 
+`GOOGLE_APPLICATION_CREDENTIALS` - Path to Google Storage credential file (json format file for service account authentication). 
 
 `GOOGLE_APPLICATION_CREDENTIALS_CONTENT` - Content of Google Storage credential file. This content is written to the file specified in `GOOGLE_APPLICATION_CREDENTIALS`. 
 
@@ -12,17 +12,23 @@ The microservice listens to the following endpoints at port 5000:
 
 `/upload` - JSON formatted config for where it is to fetch source files for upload to google storage
 
-`/entities` - returns JSON formatted entities listing all files at the google storage
-
+`/entities` - returns JSON formatted entities listing all files at the google storage  
+Available query parameters (all optional)  
+* expire: date time in format %Y-%m-%d %H:%M:%S - overrides default expire time
+* with_subfolders: False by default if assigned will include blobs from subfolders
 ```
 [
     {
         "file_id": "some-file-name",
-        "file_url": "url/to/file/"
+        "file_url": "url/to/file/" <- signed URL with expiration date
     },
     ...
 ]
 ```
+
+`/download/<bucket name>/<file path>` - downloads file from Google Storage Bucket  
+file path may include slashes to download file from bucket sub-folder
+
 
 ## Example System Config
 ```
--- service/google-storage.py
+++ service/google-storage.py
@@ -1,9 +1,10 @@
-from flask import Flask, Response, request
+from flask import Flask, Response, request, abort, send_file
 import datetime
 import json
 import os
 import logging
 from google.cloud import storage
+from io import BytesIO
 import requests
 
 app = Flask(__name__)
@@ -13,63 +14,48 @@ credentials_path = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
 credentials = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS_CONTENT")
 
 # Set up logging
-log_level = logging.getLevelName(os.environ.get("LOG_LEVEL", "INFO"))  # default log level = INFO
+log_level = logging.getLevelName(os.environ.get("LOG_LEVEL", "INFO"))
 logging.basicConfig(level=log_level)  # dump log to stdout
-format_string = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-logger = logging.getLogger("google-storage-microservice")
-
-# Log to stdout
-stdout_handler = logging.StreamHandler()
-stdout_handler.setFormatter(logging.Formatter(format_string))
-logger.addHandler(stdout_handler)
-
 
 # write out service config from env var to known file
 with open(credentials_path, "wb") as out_file:
     out_file.write(credentials.encode())
 
-# Instantiates a client
-storage_client = storage.Client()
 
-# Get all buckets in the project associated to the client
-buckets = storage_client.list_buckets()
+@app.route("/datasets/<bucket_name>/entities", methods=["GET"])
+def get_entities(bucket_name):
+    """
+    Endpoint to read entities from gcp bucket, add signed url and return
+    Available query parameters (all optional)
+        expire: date time in format %Y-%m-%d %H:%M:%S - overrides default expire time
+        with_subfolders: False by default if assigned will include blobs from subfolders
+    :return:
+    """
 
-# print("Buckets:")
-for bucket in buckets:
-    logger.debug(bucket)
-
-# get blobs in outgoing
-#incoming_blobs = bucket.list_blobs(prefix='incoming/', delimiter='/')
-#outgoing_blobs = bucket.list_blobs(prefix='outgoing/', delimiter='/')
-#blobs = bucket.list_blobs()
-
-# print('Blobs:')
-# for blob in blobs:
-#     print(blob.name)
-#     expiration = datetime.datetime(2055, 7, 14, 12, 30)
-#     encurl = blob.generate_signed_url(expiration, method='GET')
-#     print(encurl)
-
-print("done")
-
-
-# Read from metadata file for manually uploaded files
-@app.route("/entities", methods=["GET"])
-def get_entities():
+    set_expire = request.args.get('expire')
+    with_subfolders = request.args.get('with_subfolders')
 
     def generate():
+        """Lists all the blobs in the bucket."""
 
-        blobs = bucket.list_blobs(prefix="outgoing/", delimiter="/")   # FIXME: parameterize
-
+        blobs = bucket.list_blobs()
         first = True
         yield "["
         for blob in blobs:
-            # do something with the key
-            entity = {"_id": str.split(blob.name, "/")[-1]}  # keep file name only
-            if entity["_id"] == "": continue  # skip if no file name
+            entity = {"_id": blob.name}
+            if '/' in entity["_id"] and not with_subfolders:  # take only root folder
+                continue
+
+            if entity["_id"].endswith("/"):  # subfolder object
+                continue
 
             entity["file_id"] = entity["_id"]
-            expiration = datetime.datetime(2055, 7, 14, 12, 30)
+
+            if not set_expire:
+                expiration = datetime.datetime(2183, 9, 8, 13, 15)
+            else:
+                expiration = datetime.datetime.strptime(set_expire, '%Y-%m-%d %H:%M:%S')
+
             entity["file_url"] = blob.generate_signed_url(expiration, method="GET")
             entity["updated"] = str(blob.updated)
             entity["generation"] = blob.generation
@@ -79,72 +65,53 @@ def get_entities():
             yield json.dumps(entity)
             first = False
         yield "]"
-
-    return Response(generate(), mimetype="application/json")
-
-
-# download file from google storage
-# DEPRECATED
-@app.route("/download", methods=["POST"])
-def download():
-    # catch the posted data
-    file_config = request.get_json()
-
-    logger.debug("source file config: {}".format(file_config))
-
-    for entity in file_config:
-        source_file_name = entity["file_id"]
-        source_file_path = entity["file_url"]
-
-        logger.debug("source file: {}{}".format(source_file_path, source_file_name))
-
-        blob = bucket.blob(source_file_name + source_file_name)
-
-        # FIXME
-        tmp_file = "tmp"
-        with open(tmp_file, "wb") as out_file:
-            blob.download_to_filename(out_file)
-
-    return Response(json.dumps(file_config), mimetype="text/plain")
+    try:
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket(bucket_name)
+        response = Response(generate(), mimetype="application/json")
+        return response
+    except Exception as e:
+        logging.error(e.message)
+        abort(e.code, e.message)
 
 
-# upload file to google storage
-@app.route("/upload", methods=["POST"])
-def upload():
+@app.route("/download/<bucket>/<path:filename>", methods=['GET'])
+def download(bucket, filename):
+    """
+    Downloads file with given name from given bucket
+    :param bucket: Google cloud storage bucket name
+    :param filename: path to file (dsimply file name for files in root folder)
+    :return: file
+    """
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket)
+    try:
+        blob = bucket.blob(filename)
+        file_data = blob.download_as_string()
+        return send_file(BytesIO(file_data), attachment_filename=blob.name, mimetype=blob.content_type)
+    except Exception as e:
+        logging.error(e.message)
+        abort(e.code, e.message)
 
-    # google store target location
-    remote_file_path = "incoming/"  # FIXME: parameterize
 
-    # catch the posted data
-    source_file_config = request.get_json()
+@app.route("/upload/<bucket_name>", methods=["POST"])
+def upload(bucket_name):
+    """
+    Upload file to given bucket
+    :param bucket_name:
+    :return: 200 code if everyting OK
+    """
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    files = request.files
 
-    logger.debug("source file config: {}".format(source_file_config))
-
-    for entity in source_file_config:
-        # get file config for hosted files
-        source_file_name = entity["file_id"]
-        source_file_path = entity["file_url"]
-
-        logger.debug("source file: {}{}".format(source_file_path, source_file_name))
-
-        # download hosted files locally
-        r = requests.get(source_file_path, stream=True)
-
-        tmp_file = "tmp"
-        with open(tmp_file, "wb") as fd:
-            for chunk in r.iter_content(chunk_size=128):
-                fd.write(chunk)
-
-        # upload local files to google store
-        blob = bucket.blob(remote_file_path + source_file_name)
-        blob.upload_from_filename(tmp_file)
-
-        # clean up local files
-        os.remove(tmp_file)
-
-        logger.info("File {} uploaded to {}.".format(source_file_path + source_file_name, blob))
-
-    return Response(json.dumps(source_file_config), mimetype="text/plain")
+    for file in files:
+        if files[file].filename == '':
+            continue
+        filename = files[file].filename
+        blob = bucket.blob(filename)
+        blob.upload_from_file(files[file])
+    return Response()
 
 
 if __name__ == "__main__":
